51804f69619004823b57c5118f0a4816f9d7ef05
Joa-Mi <mmik70999@gmail.com>
2025-10-24 05:19:41 +0800

    Changes I made

---
+++ InformationManagement/Admin/Dashboard.vb
@@ -0,0 +1,10 @@
+﻿Public Class Dashboard
+    Private Sub Dashboard_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Dashboard_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+
+
+    End Sub
+End Class
